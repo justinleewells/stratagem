086f7d6ee93fb12fa03048ebb180ee243f8d69ec
justinleewells <justinleewells@icloud.com>
2017-01-17 22:59:06 -0700

    Implemented Resource mod functions

--- test/resource.js
+++ test/resource.js
@@ -4,4 +4,99 @@ var Resource = require('../lib/objects/resource');
 
 describe('Resource', function () {
 
+  describe('#getBase()', function () {
+
+    it('should return the base value', function (done) {
+      var resource = new Resource();
+      resource.getBase().should.equal(0);
+      done();
+    });
+
+  });
+
+  describe('#getCurrent()', function () {
+
+    it('should return the current value', function (done) {
+      var resource = new Resource();
+      resource.getCurrent().should.equal(0);
+      done();
+    });
+
+  });
+
+  describe('#getMax()', function () {
+
+    it('should return the maximum value', function (done) {
+      var resource = new Resource();
+      resource.getMaximum().should.equal(0);
+      done();
+    });
+
+  });
+
+  describe('#getMod()', function () {
+
+    it('should return the mod value', function (done) {
+      var resource = new Resource();
+      resource.getMod().should.equal(0);
+      done();
+    });
+
+  });
+
+  describe('#initialize()', function () {
+
+    it('should set the base value', function (done) {
+      var resource = new Resource();
+      resource.initialize(10);
+      resource.getBase().should.equal(10);
+      done();
+    });
+
+    it('should set the current value', function (done) {
+      var resource = new Resource();
+      resource.initialize(10);
+      resource.getCurrent().should.equal(10);
+      done();
+    });
+
+    it('should set the maximum value', function (done) {
+      var resource = new Resource();
+      resource.initialize(10);
+      resource.getMaximum().should.equal(10);
+      done();
+    });
+
+    it('should reset the mod value', function (done) {
+      var resource = new Resource();
+      resource.addMod(10);
+      resource.initialize(10);
+      resource.getMod().should.equal(0);
+      done();
+    });
+
+  });
+
+  describe('#addMod()', function () {
+
+    it('should add the mod value', function (done) {
+      config.modType = 'integer';
+      var resource = new Resource();
+      resource.initialize(10);
+      resource.addMod(50);
+      resource.getMod().should.equal(50);
+      done();
+    });
+
+    it('should recalculate the max value', function (done) {
+      config.modType = 'integer';
+      var resource = new Resource();
+      resource.initialize(10);
+      resource.addMod(50);
+      resource.getMaximum().should.equal(60);
+      done();
+    });
+
+  });
+
 });
