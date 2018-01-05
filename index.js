34db96fb1da27b3c5de95d672bc561e1bdd7d971
Justin Wells <justinleewells@justins-mbp.attlocal.net>
2018-01-04 18:23:11 -0600

    Total rewrite

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
-'use strict';
-
-// Objects
-var config = require('./lib/config');
-
-/**
- * Namespace that exposes all functions available to the programmer.
- * @namespace
- */
-const API = {
-
-  /**
-   * Allows the programmer to customize the framework
-   * @function
-   * @param {Object} opts - The configuration object
-   */
-  configure: (opts) => {
-    let keys = Object.keys(config);
-    let values = keys.reduce(function (obj, key) {
-      if (opts[key] !== undefined) obj[key] = opts[key];
-      return obj;
-    }, {});
-    for (let key in values) {
-      config[key] = values[key];
-    }
-  }
-
-};
-
-module.exports = API;
+exports.config = require('./lib/config')
+exports.Action = require('./lib/action')
+exports.ActionInfo = require('./lib/action-info')
+exports.Enum = require('./lib/enum')
+exports.Instance = require('./lib/instance')
+exports.Position = require('./lib/position')
+exports.StatusEffect = require('./lib/status-effect')
+exports.StatusEffectInfo = require('./lib/status-effect-info')
+exports.State = require('./lib/state')
+exports.StateInfo = require('./lib/state-info')
+exports.Target = require('./lib/target')
+exports.Unit = require('./lib/unit')
+exports.UnitInfo = require('./lib/unit-info')
