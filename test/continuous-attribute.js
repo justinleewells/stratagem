5eecd6eeaa4f11953fe109f1a9b77465cb5a655d
Justin Wells <justinleewells@icloud.com>
2019-09-22 10:13:38 -0500

    Make ContinuousAttribute more ergonomic

--- test/continuous-attribute.js
+++ test/continuous-attribute.js
@@ -2,6 +2,16 @@ const ContinuousAttribute = require('../lib/continuous-attribute')
 const expect = require('chai').expect
 
 describe('ContinuousAttribute', () => {
+  describe('#constructor', () => {
+    it('accepts a single number as an argument', (done) => {
+      let attribute = new ContinuousAttribute(10)
+      expect(attribute.base).to.equal(10)
+      expect(attribute.current).to.equal(10)
+      expect(attribute.max).to.equal(10)
+      expect(attribute.min).to.equal(0)
+      done()
+    })
+  })
   describe('#add', () => {
     it('increases current', (done) => {
       let attribute = new ContinuousAttribute({
