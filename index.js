534791e2df95043ef9ecf98784623f0fcc705855
Justin Wells <justinleewells@icloud.com>
2020-09-10 00:58:06 -0500

    Remove Config

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = Object.freeze({
   Action: require('./lib/action'),
   DiscreteAttribute: require('./lib/discrete-attribute'),
-  Config: require('./lib/config'),
   ContinuousAttribute: require('./lib/continuous-attribute'),
   Handler: require('./lib/handler'),
   Instance: require('./lib/instance'),
