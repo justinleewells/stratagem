c8afb29e28e6c6f9d455ee5ed4e1c5f93214b30a
Justin L. Wells <justinleewells@icloud.com>
2024-01-17 08:55:56 -0600

    Make Executor asynchronous

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 import Attribute from './lib/attribute.js'
 import Event from './lib/event.js'
-import Executor from './lib/executor.js'
 import Instance from './lib/instance.js'
 import ModifierTypes from './lib/modifier-types.js'
 import Resource from './lib/resource.js'
 import Teams from './lib/teams.js'
 import Unit from './lib/unit.js'
 
-export {
-  Attribute,
-  Event,
-  Executor,
-  Instance,
-  ModifierTypes,
-  Resource,
-  Teams,
-  Unit,
-}
+export { Attribute, Event, Instance, ModifierTypes, Resource, Teams, Unit }
