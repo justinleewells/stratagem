67a1c964b212cf7b39482502480b9c0e578d41e0
Justin L. Wells <justinleewells@icloud.com>
2026-02-27 00:59:30 -0600

    Fresh start

--- index.js
+++
@@ -1,9 +0,0 @@
-import Attribute from './lib/attribute.js'
-import Event from './lib/event.js'
-import Instance from './lib/instance.js'
-import ModifierTypes from './lib/modifier-types.js'
-import Resource from './lib/resource.js'
-import Teams from './lib/teams.js'
-import Unit from './lib/unit.js'
-
-export { Attribute, Event, Instance, ModifierTypes, Resource, Teams, Unit }
